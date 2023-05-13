5b2a5211fabeb7aebcc8d8fd96a12382c11e4423
Abdullah Al Ateqi <aa.alateqi@gmail.com>
2023-05-13 19:49:33 +0400

    renaming directory

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ var map = L.map('map', {
     maxBounds: bounds,
     maxBoundsViscosity: 1,
     minZoom: 0.5,
-    maxZoom: 6,
+    maxZoom: 4,
     // continuousWorld: true,
     // noWrap:false,
     crs: L.CRS.EPSG4326,
@@ -69,7 +69,7 @@ function formatlat(num) {
     return '0' + String(s).padEnd(5, '0');
 }
 // Grid and Coordinates
-/*
+
 
 let coordP = L.control.coordProjection({
     position: 'bottomleft',
@@ -78,7 +78,7 @@ let coordP = L.control.coordProjection({
     lngFormatter: function (lng) { lng = Math.round(lng * 1000) / 1000; return (lng >= 0) ? formatlng(lng) + 'E' : formatlng(-lng) + 'W'; },
     numDigits: 2,
 }).addTo(map);
-*/
+
 
 //Zoom
 L.control.zoom({
@@ -159,7 +159,7 @@ updateinfoBox = function (layer) {
 //Layers------------------------------------------------------------------------
 
 //Base Map
-exi = L.tileLayer('https://github.com/Al-Ateqi/NYUAD-Mars-Map/raw/main/Tiles-using-Python/Latest-Map-Tiles/{z}/{x}/{y}.png', {
+exi = L.tileLayer('https://github.com/Al-Ateqi/NYUAD-Mars-Map/raw/main/Map-Tiles/Latest-Map-Tiles/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
